22f646d5fac05fde2c3ad05e6a4c941b7cd9bbac
JosephD97 <yusifsum@gmail.com>
2026-03-23 17:50:11 +0500

    Сделал авторизацию, проверил работает

--- app/auth.py
+++ app/auth.py
@@ -1,6 +1,9 @@
 #хеширование (чтобы не хранить пароли в открытом виде)
 
 from passlib.context import CryptContext
+from datetime import datetime, timedelta
+from jose import jwt
+from .config import settings
 
 #способа шифрования bcrypt
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
@@ -13,3 +16,12 @@ def get_password_hash(password):
 # функция которая проверяет: подходит ли пароль к хешу (нужна для логина)
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
+
+def create_access_token(data:dict):
+    to_encode=data.copy()
+    #срок годности токена
+    expire=datetime.utcnow() + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    #шифрование данных с помощью секретного ключа из .env
+    encoded_jwt=jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+    return encoded_jwt
--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 from .database import engine, get_db
-from . import models, schemas, crud
+from . import models, schemas, crud, auth
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 #проверит базу и создаст таблицы, если их нет
 models.Base.metadata.create_all(bind=engine)
@@ -11,6 +14,11 @@ app = FastAPI(title="Task Manager")
 @app.get("/")
 def home():
     return {"status": "Work in progress", "project": "Task Manager"}
+
+@app.get("/users/me")
+def read_users_me(token: str = Depends(oauth2_scheme)):
+    return {"token": token}
+
 
 @app.post("/register", response_model=schemas.User)
 #response_model это мера безопасности. 
@@ -22,4 +30,18 @@ def register_user(user: schemas.UserCreate, db: Session=Depends(get_db)):
         raise HTTPException(status_code=400,detail="Email already registered")
     #если email уникальный, то создаем пользователя
     return crud.create_user(db=db, user=user)
+
+@app.post("/token", response_model=schemas.Token)
+def login_for_access_token(
+    form_data: OAuth2PasswordRequestForm=Depends(),
+    db: Session=Depends(get_db)):
+    #поиск пользователя по email
+    user=crud.get_user_by_email(db, email=form_data.username)
+    #проверка на существование такого пользователя и на совпадение пароля
+    if not user or not auth.verify_password(form_data.password, user.hashed_password):
+        raise HTTPException (status_code=401, detail="Incorrect email or password")
+    #создание токена
+    access_token = auth.create_access_token(data={"sub": user.email})
+    #возврат токена
+    return {"access_token": access_token, "token_type": "bearer"}
 
